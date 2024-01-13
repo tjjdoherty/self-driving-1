796e2fb80d9300dddfdc5f71067007731969475a
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2024-01-13 17:32:46 -0500

    lerp function to make multiple lanes, but not working with dashed lines

--- road.js
+++ road.js
@@ -4,8 +4,8 @@ class Road {
         this.width = width;
         this.laneCount = laneCount;
 
-        this.left = x - width/2;
-        this.right = x + width/2;
+        this.left=x-width/2;
+        this.right=x+width/2;
 
         // define infinity - there is one built in to JavaScript 
         // but this const stops performance issues
@@ -18,19 +18,23 @@ class Road {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for (let i = 0; i <= this.laneCount; i++) {
-            // linear interpolation - go from left to right according to a %
-            const x = lerp(
-                this.left,
-                this.right,
-                i/this.laneCount
-            );
+        for (let i=0; i<=this.laneCount;i++) {
+            const x=lerp(this.left, this.right, i/this.laneCount);
 
             ctx.beginPath();
-            ctx.moveTo(x, this.top);
+            ctx.moveTo(x, this.top)
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
+
         }
     }
+}
+
+function lerp(A,B,t) {
+    // t is a percentage between the start point A and endpoint B
+    // if t = 0; evaluates to A - first lane marking
+    // if t = 1, evaluates to B - last lane marking
+    // anything in between is your extra markings for multiple lanes
+    return (A+(B-A)*t);
 }
 
